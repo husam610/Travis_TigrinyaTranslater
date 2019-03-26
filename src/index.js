4c9116b6ac598fee01063d1fd1b7d996b5b4168c
Husam <21155338+husam610@users.noreply.github.com>
2019-03-26 16:28:00 +0100

    finalVersion

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "assets/css/nucleo-icons.css";
+import "assets/scss/blk-design-system-react.scss?v=1.0.0";
+import "assets/demo/demo.css";
+
+import Index from "views/Index.jsx";
+import TranslatorPage from "views/examples/TranslatorPage.jsx";
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/components" render={props => <Index {...props} />} />
+      <Route
+        path="/translator-page"
+        render={props => <TranslatorPage {...props} />}
+      />
+      <Redirect from="/" to="/components" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
